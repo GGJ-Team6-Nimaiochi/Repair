71b6b06b4c0ab65079647126147aa29a4195af0a
18778archer <18778archer@gmail.com>
2020-02-02 07:31:06 +0900

    ドロップ後のセットデータの変更

--- Nimaiochi/Assets/Script/DropArea.cs
+++ Nimaiochi/Assets/Script/DropArea.cs
@@ -28,7 +28,7 @@ public class DropArea : MonoBehaviour, IDropHandler
         {
             Debug.Log(gameObject.name + "に" + data.pointerDrag.name + "をドロップ");
             text.text += dragObj.text.text;
-            SelectStoryData.Instance.SetID(arrayNo, dragObj.pageContentData.id);
+            SelectStoryData.Instance.SetData(arrayNo, text.text, dragObj.pageContentData.id);
             dragObj.DestroyDragObject();
             Destroy(dragObj.gameObject);
             endAction();
