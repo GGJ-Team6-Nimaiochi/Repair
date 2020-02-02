250aa953346da6870056ba8107183c0bcf96ab56
donuts-chiba <chiba.mizuki@donuts.ne.jp>
2020-02-02 13:41:35 +0900

    update : 紙芝居開発中

--- Nimaiochi/Assets/Chiba/Scripts/Chapter.cs
+++ Nimaiochi/Assets/Chiba/Scripts/Chapter.cs
@@ -10,15 +10,15 @@ public class Chapter : MonoBehaviour
     public SpriteRenderer[] houses;
 
     // 各子豚の家の画像を変える
-    private void Awake()
+    private void Start()
     {
         if (houses.Length <= SelectStoryData.Instance.id.Length)
         {
             for (int i = 0; i < houses.Length; i++)
             {
-                if(houses[i] == null || SelectStoryData.Instance.id[i] == -1) continue;
-                StorySimulator.Instance.SelectHouses[i] = HouseSprites[SelectStoryData.Instance.id[i]];
-                houses[i].sprite = StorySimulator.Instance.SelectHouses[i];
+                if(houses[i] == null) continue;
+                StorySimulator.Instance.SelectHouses[i] = !StorySimulator.Instance.SelectHouses[i] && SelectStoryData.Instance.id[i] != -1 ? HouseSprites[SelectStoryData.Instance.id[i]] : StorySimulator.Instance.SelectHouses[i];
+                if(StorySimulator.Instance.SelectHouses[i])houses[i].sprite = StorySimulator.Instance.SelectHouses[i];
             }
         }
     }
